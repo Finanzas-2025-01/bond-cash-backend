bacebf011856c3676d6e76f0e30a93968218b5b8
Al3xiel <gabriel280104casas@gmail.com>
2025-06-18 18:43:38 -0500

    chore: setup project.

---
+++ src/main/java/nrg/inc/koutape/KoutaPeApplication.java
@@ -0,0 +1,13 @@
+package nrg.inc.koutape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoutaPeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoutaPeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nrg/inc/koutape/KoutaPeApplicationTests.java
@@ -0,0 +1,13 @@
+package nrg.inc.koutape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoutaPeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

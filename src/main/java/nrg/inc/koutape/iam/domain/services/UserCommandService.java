76ec9371863a4a5d837e4d42b1f18d40f84c1536
Al3xiel <gabriel280104casas@gmail.com>
2025-06-20 01:02:47 -0500

    feat(user): add handling for CreateUserBondHolderCommand and CreateUserInvestorCommand in UserCommandService

--- src/main/java/nrg/inc/koutape/iam/domain/services/UserCommandService.java
+++ src/main/java/nrg/inc/koutape/iam/domain/services/UserCommandService.java
@@ -1,6 +1,8 @@
 package nrg.inc.koutape.iam.domain.services;
 
 import nrg.inc.koutape.iam.domain.model.aggregates.User;
+import nrg.inc.koutape.iam.domain.model.commands.CreateUserBondHolderCommand;
+import nrg.inc.koutape.iam.domain.model.commands.CreateUserInvestorCommand;
 import nrg.inc.koutape.iam.domain.model.commands.SignInCommand;
 import nrg.inc.koutape.iam.domain.model.commands.SignUpCommand;
 import org.apache.commons.lang3.tuple.ImmutablePair;
@@ -10,4 +12,6 @@ import java.util.Optional;
 public interface UserCommandService {
   Optional<ImmutablePair<User, String>> handle(SignInCommand command);
   Optional<User> handle(SignUpCommand command);
+  Optional<User> handle(CreateUserInvestorCommand command);
+  Optional<User> handle(CreateUserBondHolderCommand command);
 }

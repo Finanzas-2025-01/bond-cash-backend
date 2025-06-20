64d3ecd2a67c8e2254874dbd7cff5c90a03f8407
Al3xiel <gabriel280104casas@gmail.com>
2025-06-20 01:01:57 -0500

    feat(bonds): add CreateBondHolderCommand and CreateInvestorCommand records

---
+++ src/main/java/nrg/inc/koutape/bonds/domain/model/commands/CreateInvestorCommand.java
@@ -0,0 +1,8 @@
+package nrg.inc.koutape.bonds.domain.model.commands;
+
+import nrg.inc.koutape.bonds.domain.model.aggregates.Investor;
+
+import java.util.Optional;
+
+public record CreateInvestorCommand() {
+}

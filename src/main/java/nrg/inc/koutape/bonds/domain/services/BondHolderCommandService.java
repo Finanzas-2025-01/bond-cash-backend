f5293a7eee6f8cbb7bae7a31022a85cc35d772ec
Al3xiel <gabriel280104casas@gmail.com>
2025-06-20 01:02:21 -0500

    feat(bonds): add BondHolderCommandService and InvestorCommandService interfaces

---
+++ src/main/java/nrg/inc/koutape/bonds/domain/services/BondHolderCommandService.java
@@ -0,0 +1,10 @@
+package nrg.inc.koutape.bonds.domain.services;
+
+import nrg.inc.koutape.bonds.domain.model.aggregates.BondHolder;
+import nrg.inc.koutape.bonds.domain.model.commands.CreateBondHolderCommand;
+
+import java.util.Optional;
+
+public interface BondHolderCommandService {
+    Optional<BondHolder> handle(CreateBondHolderCommand createBondCommand);
+}

9d0607d5a26bd359e875ac8552b8f72e55fffd93
Al3xiel <gabriel280104casas@gmail.com>
2025-06-20 22:58:29 -0500

    feat(bond): add CreateBondResource for bond creation data transfer

---
+++ src/main/java/nrg/inc/koutape/bonds/interfaces/rest/resources/CreateBondResource.java
@@ -0,0 +1,28 @@
+package nrg.inc.koutape.bonds.interfaces.rest.resources;
+
+import nrg.inc.koutape.bonds.domain.model.valueobjects.Capitalization;
+import nrg.inc.koutape.bonds.domain.model.valueobjects.CuponFrequency;
+import nrg.inc.koutape.bonds.domain.model.valueobjects.InterestRateType;
+
+import java.util.Date;
+
+public record CreateBondResource(
+        String name,
+        Double nominalValue,
+        Double comercialValue,
+        Integer years,
+        CuponFrequency cuponFrequency,
+        InterestRateType interestRateType,
+        Capitalization capitalization,
+        Double interestRatePercentage,
+        Double anualDiscountRatePercentage,
+        Double incomeTaxPercentage,
+        Date issueDate,
+        Double premiumPercentage,
+        Double structuringPercentage,
+        Double placementPercentage,
+        Double floatingRatePercentage,
+        Double CAVALIPercentage,
+        Double anualInflationPercentage
+) {
+}

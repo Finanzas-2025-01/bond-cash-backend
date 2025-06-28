3f47ee2ce2c6fbf3244f8b079ed85f31a3851001
Al3xiel <gabriel280104casas@gmail.com>
2025-06-27 20:17:06 -0500

    feat(bond): add cash flow grace periods and enhance cash flow generation logic

--- src/main/java/nrg/inc/koutape/bonds/domain/model/aggregates/Bond.java
+++ src/main/java/nrg/inc/koutape/bonds/domain/model/aggregates/Bond.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import nrg.inc.koutape.bonds.domain.model.commands.CreateBondCommand;
+import nrg.inc.koutape.bonds.domain.model.entities.CashFlowGracePeriod;
 import nrg.inc.koutape.bonds.domain.model.valueobjects.Capitalization;
 import nrg.inc.koutape.bonds.domain.model.valueobjects.CuponFrequency;
 import nrg.inc.koutape.bonds.domain.model.valueobjects.GracePeriod;
 import nrg.inc.koutape.bonds.domain.model.valueobjects.InterestRateType;
 import nrg.inc.koutape.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -71,6 +73,9 @@ public class Bond extends AuditableAbstractAggregateRoot<Bond> {
     @OneToOne(mappedBy = "bond", cascade = CascadeType.ALL, orphanRemoval = true)
     private BondResult bondResult;
 
+    @OneToMany(mappedBy = "bond", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CashFlowGracePeriod> cashFlowGracePeriods;
+
     public Bond() {
         // Default constructor
     }
@@ -96,7 +101,39 @@ public class Bond extends AuditableAbstractAggregateRoot<Bond> {
         this.anualInflationPercentage = command.anualInflationPercentage();
     }
 
+    public void generateCashFlowGracePeriods() {
+        if (this.cashFlowGracePeriods == null) {
+            this.cashFlowGracePeriods = new ArrayList<>();
+        } else {
+            this.cashFlowGracePeriods.clear();
+        }
+        var cuponFrequencyValue = 0;
+        switch (this.cuponFrequency) {
+            case MONTHLY -> cuponFrequencyValue = 30;
+            case BIMONTHLY -> cuponFrequencyValue = 60;
+            case TRIMESTRAL -> cuponFrequencyValue = 90;
+            case QUADRIMONTHLY -> cuponFrequencyValue = 120;
+            case SEMIANNUAL -> cuponFrequencyValue = 180;
+            case ANNUAL -> cuponFrequencyValue = 360;
+        }
+        var periodsPerYear = this.daysPerYear / cuponFrequencyValue;
+        var totalPeriods = this.years * periodsPerYear;
+
+        for (int i = 0; i < totalPeriods; i++) {
+            var cashFlowGracePeriod = new CashFlowGracePeriod();
+            cashFlowGracePeriod.setBond(this);
+            cashFlowGracePeriod.setPeriodNumber(i + 1); // Period numbers start from 1
+            cashFlowGracePeriod.setGracePeriod(GracePeriod.S); // Valor por defecto
+            this.cashFlowGracePeriods.add(cashFlowGracePeriod);
+        }
+    }
+
     public void generateCashFlows(){
+        if (this.cashFlows == null) {
+            this.cashFlows = new ArrayList<>();
+        } else {
+            this.cashFlows.clear();
+        }
         var cuponFrequencyValue = 0;
         switch (this.cuponFrequency) {
             case MONTHLY -> cuponFrequencyValue = 30;
@@ -123,12 +160,22 @@ public class Bond extends AuditableAbstractAggregateRoot<Bond> {
         var effectiveAnualRate = 0.0;
         switch (this.interestRateType) {
             case EFFECTIVE -> effectiveAnualRate = this.interestRatePercentage/100;
-            case NOMINAL -> effectiveAnualRate = (Math.pow(1 + (this.interestRatePercentage/( (double)this.daysPerYear/capitalizationDays)), (double)this.daysPerYear/capitalizationDays) - 1)/100;
+            case NOMINAL -> effectiveAnualRate = (Math.pow(1 + ((this.interestRatePercentage/100)/((double)this.daysPerYear/capitalizationDays)), (double)this.daysPerYear/capitalizationDays) - 1);
         }
-        var effectivePeriodRate = (Math.pow((1+effectiveAnualRate), (double)cuponFrequencyValue/this.daysPerYear))/100;
-        var periodCOK = (Math.pow((1+this.anualDiscountRatePercentage/100), (double)cuponFrequencyValue/this.daysPerYear))/100;
+        var effectivePeriodRate = (Math.pow((1+effectiveAnualRate), (double)cuponFrequencyValue/this.daysPerYear)) - 1;
+        var periodCOK = (Math.pow((1+this.anualDiscountRatePercentage/100), (double)cuponFrequencyValue/this.daysPerYear)) - 1;
         var initialIssuerCosts = ((this.structuringPercentage+this.placementPercentage+this.floatingRatePercentage+this.CAVALIPercentage)/100) * this.comercialValue;
         var initialBondHolderCosts = ((this.floatingRatePercentage+this.CAVALIPercentage)/100) * this.comercialValue;
+
+        System.out.println("cuponFrequencyValue: " + cuponFrequencyValue);
+        System.out.println("capitalizationDays: " + capitalizationDays);
+        System.out.println("periodsPerYear: " + periodsPerYear);
+        System.out.println("totalPeriods: " + totalPeriods);
+        System.out.println("effectiveAnualRate: " + effectiveAnualRate);
+        System.out.println("effectivePeriodRate: " + effectivePeriodRate);
+        System.out.println("periodCOK: " + periodCOK);
+        System.out.println("initialIssuerCosts: " + initialIssuerCosts);
+        System.out.println("initialBondHolderCosts: " + initialBondHolderCosts);
         for (int i = 0; i <= totalPeriods; i++) {
             var cashFlow = new CashFlow();
             cashFlow.setBond(this);
@@ -139,26 +186,115 @@ public class Bond extends AuditableAbstractAggregateRoot<Bond> {
                 cashFlow.setIssuerFlowWithShield(this.comercialValue - initialIssuerCosts);
                 cashFlow.setBondHolderFlow(-this.comercialValue - initialBondHolderCosts);
             }else {
+                var previousCashFlow = this.cashFlows.get(i - 1);
+                //Assignated date
                 // Calculate the assigned date based on the previous cash flow's assigned date and the cupon frequency
-                var previousAssignedDate = this.cashFlows.get(i - 1).getAssignedDate();
+                var previousAssignedDate = previousCashFlow.getAssignedDate();
                 var calendar = Calendar.getInstance();
                 calendar.setTime(previousAssignedDate);
                 calendar.add(Calendar.DAY_OF_YEAR, cuponFrequencyValue);
                 var assignedDate = calendar.getTime();
                 cashFlow.setAssignedDate(assignedDate);
 
+                // Anual inflation
                 // Set the annual inflation
                 cashFlow.setAnualInflation(this.anualInflationPercentage/100);
 
+                // Period inflation
                 // Set the period inflation based on the cupon frequency
                 var cashFlowAnualInflation = this.anualInflationPercentage/100;
-                System.out.println("Anual Inflation: " + cashFlowAnualInflation + " for period: " + i);
                 var periodInflation = Math.pow(1 + cashFlowAnualInflation,( (double) cuponFrequencyValue/this.daysPerYear))-1;
-                System.out.println("Period Inflation: " + periodInflation + " for period: " + i);
                 cashFlow.setPeriodInflation(periodInflation);
 
-                // Set the cupon grace period
-                cashFlow.setGracePeriod(GracePeriod.S);
+                // Grace Period
+                // Set the cupon grace period+
+                int periodNumber = i;
+                GracePeriod gracePeriod = this.cashFlowGracePeriods.stream()
+                        .filter(gp -> gp.getPeriodNumber() != null && gp.getPeriodNumber().intValue() == periodNumber)
+                        .findFirst()
+                        .map(CashFlowGracePeriod::getGracePeriod)
+                        .orElse(GracePeriod.S);
+                cashFlow.setGracePeriod(gracePeriod);
+
+                //Bond
+                if(i == 1){
+                    cashFlow.setBondValue(this.nominalValue);
+                }else{
+                    double bondValue;
+                    if(previousCashFlow.getGracePeriod() == GracePeriod.T) {
+                        bondValue = previousCashFlow.getIndexedBondValue() - previousCashFlow.getCupon();
+                    }else{
+                        bondValue = previousCashFlow.getIndexedBondValue() + previousCashFlow.getAmortization();
+                    }
+                    cashFlow.setBondValue(bondValue);
+                }
+
+                //Indexed Bond Value
+                cashFlow.setIndexedBondValue(cashFlow.getBondValue() * (1 + cashFlow.getPeriodInflation()));
+
+                //Cupon
+                cashFlow.setCupon(cashFlow.getIndexedBondValue() * effectivePeriodRate * -1);
+
+                //Payment
+                switch (cashFlow.getGracePeriod()){
+                    case T:
+                        cashFlow.setPayment(0.0);
+                        System.out.println("Payment is 0 due to grace period " + cashFlow.getGracePeriod() + " for period " + i);
+                        break;
+                    case P:
+                        cashFlow.setPayment(cashFlow.getCupon());
+                        System.out.println("Payment is cupon: " + cashFlow.getPayment() + " due to grace period " + cashFlow.getGracePeriod() + " for period " + i);
+                        break;
+                    case S:
+                        double paymentNumerator = cashFlow.getIndexedBondValue() * effectivePeriodRate;
+                        double paymentDenominator = 1 - Math.pow(1 + effectivePeriodRate, - (totalPeriods - i + 1));
+                        double payment = paymentNumerator / paymentDenominator;
+                        cashFlow.setPayment(payment * - 1);
+                        System.out.println("Payment is calculated: " + cashFlow.getPayment() + " due to grace period " + cashFlow.getGracePeriod() + " for period " + i);
+                        break;
+                }
+
+                //Amortization
+                if(cashFlow.getGracePeriod() == GracePeriod.S) {
+                    cashFlow.setAmortization(cashFlow.getPayment() - cashFlow.getCupon());
+                }else {
+                    cashFlow.setAmortization(0.0);
+                }
+
+                //Premium
+                if(i == totalPeriods) {
+                    cashFlow.setPremium(-1 * cashFlow.getIndexedBondValue() * this.premiumPercentage / 100);
+                } else {
+                    cashFlow.setPremium(0.0);
+                }
+
+                //Shield
+                cashFlow.setShield(-1 * cashFlow.getCupon() * this.incomeTaxPercentage / 100);
+
+                //Issuer Flow
+                cashFlow.setIssuerFlow(cashFlow.getPayment() + cashFlow.getPremium());
+
+                //Issuer Flow with Shield
+                cashFlow.setIssuerFlowWithShield(cashFlow.getPayment() + cashFlow.getPremium() + cashFlow.getShield());
+
+                //Bond Holder Flow
+                cashFlow.setBondHolderFlow((cashFlow.getPayment() + cashFlow.getPremium()) * -1);
+
+                //Updated Flow
+                cashFlow.setUpdatedFlow(
+                    cashFlow.getBondHolderFlow() * Math.pow(1 + periodCOK, -i)
+                );
+
+                //FA x Period
+                cashFlow.setFAxPeriod(
+                    cashFlow.getUpdatedFlow() * i * cuponFrequencyValue / this.daysPerYear
+                );
+
+                //P Factor
+                cashFlow.setPFactor(
+                    cashFlow.getUpdatedFlow() * i * (1 + i)
+                );
+
             }
             cashFlows.add(cashFlow);
         }

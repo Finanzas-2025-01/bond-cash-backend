9bd8ac8fe68f29bb270188be79a3343ee33210cc
Al3xiel <gabriel280104casas@gmail.com>
2025-07-04 01:19:31 -0500

    feat(auth): add roles to AuthenticatedUserResource and update assembler logic

--- src/main/java/nrg/inc/koutape/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.java
+++ src/main/java/nrg/inc/koutape/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.java
@@ -1,15 +1,20 @@
 package nrg.inc.koutape.iam.interfaces.rest.transform;
 
 import nrg.inc.koutape.iam.domain.model.aggregates.User;
+import nrg.inc.koutape.iam.domain.model.entities.Role;
 import nrg.inc.koutape.iam.interfaces.rest.resources.AuthenticatedUserResource;
 
 public class AuthenticatedUserResourceFromEntityAssembler {
 
   public static AuthenticatedUserResource toResourceFromEntity(User user, String token) {
+    var roles = user.getRoles().stream()
+            .map(Role::getStringName)
+            .toList();
     return new AuthenticatedUserResource(
             user.getId(),
             user.getUsername(),
-            token
+            token,
+            roles
     );
   }
 }
